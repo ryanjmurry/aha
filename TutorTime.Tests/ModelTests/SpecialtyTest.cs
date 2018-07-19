b562f830d5e978c203d89a031e7e112e5e8a9f2d
Ryan Murry <ryanjmurry@gmail.com>
2018-07-19 15:54:51 -0700

    add properties and constructor for Appointment Model. Add Equals, GetAll, DeleteAll, Save, Find, Delete, and Update methods and corresponding tests

--- TutorTime.Tests/ModelTests/SpecialtyTest.cs
+++ TutorTime.Tests/ModelTests/SpecialtyTest.cs
@@ -6,8 +6,93 @@ using TutorTime.Models;
 namespace TutorTime.Tests
 {
     [TestClass]
-    public class SpecialtyTest
+    public class SpecialtyTest : IDisposable
     {
-       
+       public void Dispose()
+        {
+            Specialty.DeleteAll();
+        }
+
+        public SpecialtyTest()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=rjm_test;";
+        }
+
+        [TestMethod]
+        public void GetSet_GetsAndSetsSpecialtyProperties_Properties()
+        {
+            Specialty newSpecialty = new Specialty("Chemistry", "Science", 1);
+            Assert.AreEqual("Chemistry", newSpecialty.Subject);
+            Assert.AreEqual("Science", newSpecialty.Discipline);
+            Assert.AreEqual(1, newSpecialty.Id);
+        }
+
+        [TestMethod]
+        public void Equals_ReturnsTrueIfPropertiesAreSame_True()
+        {
+            Specialty newSpecialty1 = new Specialty("Chemistry", "Science", 1);
+            Specialty newSpecialty2 = new Specialty("Chemistry", "Science", 1);
+            Assert.AreEqual(newSpecialty1, newSpecialty2);
+        }
+
+        [TestMethod]
+        public void GetAll_ChecksDbStartsEmpty_0()
+        {
+            int result = Specialty.GetAll().Count;
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Save_SavesSpecialtyToDb_SpecialtyList()
+        {
+            Specialty newSpecialty = new Specialty("Chemistry", "Science");
+            newSpecialty.Save();
+            List<Specialty> expectedList = new List<Specialty> { newSpecialty };
+            List<Specialty> actualList = Specialty.GetAll();
+            CollectionAssert.AreEqual(expectedList, actualList);
+        }
+
+        [TestMethod]
+        public void DeleteAll_DeletesAllSpecialtysFromDb_SpecialtyList()
+        {
+            Specialty newSpecialty = new Specialty("Chemistry", "Science");
+            newSpecialty.Save();
+            Specialty.DeleteAll();
+            List<Specialty> expectedList = new List<Specialty> { };
+            List<Specialty> actualList = Specialty.GetAll();
+            CollectionAssert.AreEqual(expectedList, actualList);
+        }
+
+        [TestMethod]
+        public void Find_FindsSpecialtyInDb_Specialty()
+        {
+            Specialty newSpecialty = new Specialty("Chemistry", "Science");
+            newSpecialty.Save();
+            Specialty foundSpecialty = Specialty.Find(newSpecialty.Id);
+            Assert.AreEqual(newSpecialty, foundSpecialty);
+        }
+
+        [TestMethod]
+        public void Delete_DeletesSpecialtyInDb_SpecialtyList()
+        {
+            Specialty newSpecialty1 = new Specialty("Chemistry", "Science");
+            newSpecialty1.Save();
+            Specialty newSpecialty2 = new Specialty("Chemistry", "Science");
+            newSpecialty2.Save();
+            Specialty.Delete(newSpecialty1.Id);
+            List<Specialty> expectedList = new List<Specialty> { newSpecialty2 };
+            List<Specialty> actualList = Specialty.GetAll();
+            CollectionAssert.AreEqual(expectedList, actualList);
+        }
+
+        [TestMethod]
+        public void Update_UpdateSpecialtyFromDb_String()
+        {
+            Specialty newSpecialty = new Specialty("Chemistry", "Science");
+            newSpecialty.Save();
+            newSpecialty.Update("Biology", "Science", newSpecialty.Id);
+            string actualSubject = Specialty.Find(newSpecialty.Id).Subject;
+            Assert.AreEqual("Biology", actualSubject);
+        }
     }
 }
--- TutorTime/Models/Specialty.cs
+++ TutorTime/Models/Specialty.cs
@@ -7,6 +7,159 @@ namespace TutorTime.Models
 {
     public class Specialty
     {
-    
+        public int Id { get; set; }
+        public string Subject { get; set; }
+        public string Discipline { get; set; }
+
+        public Specialty(string subject, string discipline, int id = 0)
+        {
+            Id = id;
+            Subject = subject;
+            Discipline = discipline;
+        }
+
+        public override bool Equals(System.Object otherSpecialty)
+        {
+            if (!(otherSpecialty is Specialty))
+            {
+                return false;
+            }
+            else
+            {
+                Specialty newSpecialty = (Specialty) otherSpecialty;
+                bool idEquality = (this.Id == newSpecialty.Id);
+                bool subjectEquality = (this.Subject == newSpecialty.Subject);
+                bool disciplinequality = (this.Discipline == newSpecialty.Discipline);
+                return (idEquality && subjectEquality && disciplinequality);
+            }
+        }
+
+        public static List<Specialty> GetAll()
+        {
+            List<Specialty> allSpecialties = new List<Specialty> { };
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM specialties;";
+            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while (rdr.Read())
+            {
+                int specialtyId = rdr.GetInt32(0);
+                string specialtySubject = rdr.GetString(1);
+                string specialtyDiscipline = rdr.GetString(2);
+                Specialty newSpecialty = new Specialty (specialtySubject, specialtyDiscipline, specialtyId);
+                allSpecialties.Add(newSpecialty);
+            }
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return allSpecialties;
+        }
+
+        public static void DeleteAll()
+        {
+            List<Specialty> allSpecialties = new List<Specialty> { };
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM specialties;";
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public void Save()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"INSERT INTO specialties (subject, discipline) VALUES (@specialtySubject, @specialtyDiscipline);";
+            cmd.Parameters.AddWithValue("@specialtySubject", this.Subject);
+            cmd.Parameters.AddWithValue("@specialtyDiscipline", this.Discipline);
+            cmd.ExecuteNonQuery();
+
+            Id = (int) cmd.LastInsertedId;
+
+            conn.Close();
+            if(conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public static Specialty Find(int id)
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM specialties WHERE id = @specialtyId;";
+            cmd.Parameters.AddWithValue("@specialtyId", id);
+            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+        
+                int specialtyId = 0;
+                string specialtySubject = "";
+                string specialtyDiscipline = "";
+
+            while(rdr.Read())
+            {
+                specialtyId = rdr.GetInt32(0);
+                specialtySubject = rdr.GetString(1);
+                specialtyDiscipline = rdr.GetString(2);
+            }
+
+            Specialty foundSpecialty = new Specialty (specialtySubject, specialtyDiscipline, specialtyId);
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Close();
+            }
+
+            return foundSpecialty;
+        }
+
+        public static void Delete(int id)
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM specialties WHERE id = @specialtyId;";
+            cmd.Parameters.AddWithValue("@specialtyId", id);
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public void Update(string subject, string discipline, int id)
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"UPDATE specialties SET subject = @specialtySubject, discipline = @specialtyDiscipline WHERE id = @specialtyId;";
+            cmd.Parameters.AddWithValue("@specialtySubject", subject);
+            cmd.Parameters.AddWithValue("@specialtyDiscipline", discipline);
+            cmd.Parameters.AddWithValue("@specialtyId", id);
+            cmd.ExecuteNonQuery();
+
+            Id = id;
+            Subject = subject;
+            Discipline = discipline;
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
     }
 }
+
