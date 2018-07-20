7d04fb09ca86355db5ae231c5f55caf9fc07439c
Ryan Murry <ryanjmurry@gmail.com>
2018-07-20 03:37:16 -0700

    add tutor specialty page and update tutor controller

--- Aha/Controllers/TutorsController.cs
+++ Aha/Controllers/TutorsController.cs
@@ -12,7 +12,22 @@ namespace Aha.Controllers
         [HttpGet("/tutors/new")]
         public ActionResult Form()
         {
+            return View();
+        }
+
+        [HttpPost("/tutors/new")]
+        public ActionResult Create(string tutorFirstName, string tutorLastName, string tutorEmail, string tutorPhoneNumber, int tutorExperience, bool tutorCredential, string tutorAvailability, double tutorRate)
+        {
+            Tutor newTutor = new Tutor(tutorFirstName, tutorLastName, tutorEmail, tutorPhoneNumber, tutorExperience, tutorCredential, tutorAvailability, tutorRate);
+            newTutor.Save();
+            return RedirectToAction("Specialties", new { id = newTutor.Id});
+        }
+
+        [HttpGet("/tutors/{id}/specialties")]
+        public ActionResult Specialties(int id)
+        {
             ViewModel newViewModel = new ViewModel();
+            newViewModel.FindTutor(id);
             return View(newViewModel);
         }
     }
