9c16b4496ffd80093acfa9f933b3caa6b8ec4795
Ryan Murry <ryanjmurry@gmail.com>
2018-07-20 12:20:37 -0700

    add new client form view page and needs view page. Update Client controller respectively

--- Aha/Controllers/ClientsController.cs
+++ Aha/Controllers/ClientsController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Aha.Models;
+using Aha.ViewModels;
 
 
 namespace Aha.Controllers
@@ -12,6 +13,31 @@ namespace Aha.Controllers
         public ActionResult Form()
         {
             return View();
+        }
+
+        [HttpPost("/clients/new")]
+        public ActionResult Create(string clientFirstName, string clientLastName, string clientEmail, string clientPhoneNumber, string clientStreetAddress, string clientCity, string clientState, string clientZip, DateTime clientBirthday)
+        {
+            Client newClient = new Client(clientFirstName, clientLastName, clientEmail, clientPhoneNumber, clientStreetAddress, clientCity, clientState, clientZip, clientBirthday);
+            newClient.Save();
+            return RedirectToAction("Needs", new { id = newClient.Id});
+        }
+
+        [HttpGet("/clients/{id}/needs")]
+        public ActionResult Needs(int id)
+        {
+            ViewModel newViewModel = new ViewModel();
+            newViewModel.FindClient(id);
+            return View(newViewModel);
+        }
+
+        [HttpPost("/clients/{id}/needs")]
+        public ActionResult AddNeed(int id, int clientNeed)
+        {
+            Client currentClient = Client.Find(id);
+            Specialty newNeed = Specialty.Find(clientNeed);
+            currentClient.AddNeed(newNeed);
+            return RedirectToAction("Needs");
         }
     }
 }
